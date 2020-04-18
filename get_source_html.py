eee5a74b0b78537488f5c7cac738007c8b7112eb
Alex Snow <alexsnow348@gmail.com.my>
2020-04-18 13:49:22 +0800

    added all the data

--- get_source_html.py
+++ get_source_html.py
@@ -10,21 +10,24 @@ chrome_options = Options()
 chrome_options.add_argument('--headless')
 chrome_options.add_argument('--no-sandbox')
 chrome_options.add_argument('--disable-dev-shm-usage')
-driver = webdriver.Chrome('/usr/local/bin/chromedriver',chrome_options=chrome_options)
+driver = webdriver.Chrome('/usr/local/bin/chromedriver',
+                          chrome_options=chrome_options)
 
-driver.get("https://doph.maps.arcgis.com/apps/opsdashboard/index.html#/f8fb4ccc3d2d42c7ab0590dbb3fc26b8")
+driver.get(
+    "https://doph.maps.arcgis.com/apps/opsdashboard/index.html#/f8fb4ccc3d2d42c7ab0590dbb3fc26b8")
 print(driver.title)
 
 
 # Wait 30 seconds for page to load and extract the element after it loads
 timeout = 30
 try:
-    WebDriverWait(driver, timeout).until(EC.visibility_of_element_located((By.ID, "ember6")))
+    WebDriverWait(driver, timeout).until(
+        EC.visibility_of_element_located((By.ID, "ember8")))
 except TimeoutException:
     print('Timed out waiting for page to load')
     driver.quit()
 
-Html_file= open("source_html.html","w")
+Html_file = open("source_html_2.html", "w")
 Html_file.write(driver.page_source)
 Html_file.close()
 driver.close()
